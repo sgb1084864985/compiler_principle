7b5ba03d75d6a9f82cd35bb2b672b5f74d47b0b6
lzy <lzy@qq.com>
2022-05-18 13:27:19 +0800

    Csymbols

--- usr-defined/attributes/Csymbols.hpp
+++ usr-defined/attributes/Csymbols.hpp
@@ -86,7 +86,8 @@ namespace CSym {
     using external_declaration=NonTerminal;
     using function_definition=NonTerminal;
     using declaration_list=NonTerminal;
-
+    using or_expr=inclusive_or_expr;
+    using xor_expr=exclusive_or_expr;
 
 //    enum enumSymbol{
 //        PrimaryExpr
