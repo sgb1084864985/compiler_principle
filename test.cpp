fbabfe4a185ba7ecb2e86943c98ceebc5f7230b2
lzy <lzy@qq.com>
2022-05-18 13:30:02 +0800

    modified

--- test.cpp
+++ test.cpp
@@ -4,20 +4,8 @@
 #include <iostream>
 #include <memory>
 
-#include "llvm/Pass.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Value.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/raw_ostream.h"
 
 int main(){
-
-    auto context = std::make_unique<llvm::LLVMContext>();
-    std::unique_ptr<llvm::IRBuilder<>> builder;
-    auto p=builder->CreateLoad(nullptr, nullptr);
+    std::cout<<"hello world"<<std::endl;
     return 0;
 }
